33ee887250edd1b3c1ac771fdf88723a962475d4
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-09-17 06:18:02 +0000

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './Components/Login'
 import Register from './Components/Register'
 import Home from './Components/Home'
 import AddProduct from './Components/AddProduct'
-const App = ({element: Component, ...rest}) => {
+const App = () => {
   const isLoggedIn = !!localStorage.getItem('token');
   return (
     <Router>
@@ -12,8 +12,8 @@ const App = ({element: Component, ...rest}) => {
         <Routes>
           <Route path='/' element={<Login/>}/>
           <Route  path='/register' element={<Register/>}/>
-          <Route  path='/home' {...rest} element={isLoggedIn ? <Home/> : <Navigate to='/'/>}/>
-          <Route  path='/add-project' {...rest} element={isLoggedIn ? <AddProduct/> : <Navigate to='/'/>}/>
+          <Route  path='/home'  element={isLoggedIn ? <Home/> : <Navigate to='/'/>}/>
+          <Route  path='/add-project'  element={isLoggedIn ? <AddProduct/> : <Navigate to='/'/>}/>
         </Routes> 
     </div>
     </Router>
