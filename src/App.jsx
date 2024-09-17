9221218ff4aedccd8fb243d79532234cef42fd91
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2024-09-17 08:50:45 +0000

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router,Routes,Route,Navigate,NavigationType} from 'react-router-dom'
+import {BrowserRouter, Route, Routes,Navigate} from 'react-router-dom'
 import Login from './Components/Login'
 import Register from './Components/Register'
 import Home from './Components/Home'
@@ -7,16 +7,16 @@ import AddProduct from './Components/AddProduct'
 const App = () => {
   const isLoggedIn = !!localStorage.getItem('token');
   return (
-    <Router>
-    <div> 
-        <Routes>
+    <>
+    <BrowserRouter>
+    <Routes>
           <Route path='/' element={<Login/>}/>
           <Route  path='/register' element={<Register/>}/>
           <Route  path='/home'  element={isLoggedIn ? <Home/> : <Navigate to='/'/>}/>
           <Route  path='/add-project'  element={isLoggedIn ? <AddProduct/> : <Navigate to='/'/>}/>
         </Routes> 
-    </div>
-    </Router>
+    </BrowserRouter>
+    </>
   )
 }
 
